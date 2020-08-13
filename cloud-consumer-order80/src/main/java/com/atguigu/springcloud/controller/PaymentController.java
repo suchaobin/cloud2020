911a9a9365e894611d34a30a9ff0094464019718
suchaobin <389538096@qq.com>
2020-08-13 11:59:25 +0800

    修改配置信息

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import sun.net.www.http.HttpClient;
-
-import java.net.URL;
 
 /**
  * @author suchaobin
