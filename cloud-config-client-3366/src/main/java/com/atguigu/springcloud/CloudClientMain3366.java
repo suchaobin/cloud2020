4441c9944c1a9c57dce9fa61b17f3b6020e78af6
suchaobin <389538096@qq.com>
2020-12-10 22:58:18 +0800

    bus 消息总线 设计思想：修改配置后，通过通知config的server端，来通知其他的所有client端 还可以使用定点通知，只要在原监控路径后加上定点通知的微服务名：对应端口

--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/CloudClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/CloudClientMain3366.java
@@ -7,12 +7,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
  * @author suchaobin
  * @description 启动类
- * @date 2020/12/9 10:55
+ * @date 2020/12/10 21:49
  **/
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355 {
+public class CloudClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class, args);
+        SpringApplication.run(CloudClientMain3366.class, args);
     }
 }
